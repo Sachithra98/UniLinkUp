35322dcb82249ef8b9ddc0f6732a75d5438bab37
Sachithra98 <sachithraw98@gmail.com>
2023-10-12 01:25:09 +0530

    update login and make it as the home structure

--- UniLinkUp/resources/views/login.blade.php
+++ UniLinkUp/resources/views/login.blade.php
@@ -1,310 +1,289 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 	<title>Login Form</title>
 
-	<!-- custom CSS file link -->
 	<link rel="stylesheet" href="{{ asset('css/sty.css') }}">
-
-	<!-- swiper CSS link -->
 	<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-
-	<!-- Font Awesome CDN link -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-	<!-- Bootstrap links -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-	<!-- Your custom font links (if any) -->
-	<!-- <link rel="stylesheet" href="your-font-link-here"> -->
+	<style>
+		.lgin {
+			position: relative;
+			width: 100%;
+			min-height: 100vh;
+			display: flex;
+			justify-content: space-between;
+			background-color: #eee;
+		}
 
-<style>
-   
+		.lgin .imgBx {
+			flex: 1;
+			padding: 20px;
+			text-align: center;
+			display: flex;
+			flex-direction: column;
+			align-items: center; /* Center horizontally */
+    		justify-content: center; /* Center vertically */
+		}
 
+		.lgin .imgBx img {
+			max-width: 100%;
+			height: auto;
+			display: block;
+			width: 100%;
+		}
 
+		.lgin .text1 h1 {
+			color:var(--white);
+			text-transform: uppercase;
+			padding:1rem 0;
+			font-size: 4vw;
+			text-decoration: none;
 
-.login{
-	position:relative;
-	width: 100%;
-	height: 100vh;
-	display: flex;
-}
+		}
 
-.login .imgBx{
-	position:relative;
-	width: 200px;
-	height: flex;
-}
+		.lgin .text1 h3 {
+			color: #D2F9F9;
+			text-decoration: none;
 
-.login .imgBx img {
-    position: absolute;
-    top: 100px;
-    margin-left: 430px;
-    width: 370px;
-    height: 450px;
-}
+		}
 
-.login .text h4{
+		.lgin .box1 {
+			flex: 1;
+			padding: 20px;
+			text-align: center;
+			position: relative;
+			background: linear-gradient(60deg, #453fff, transparent, transparent, #453fff);
+			background-size: 300% 300%;
+			background-position: 50% 50%;
+			animation: animate 6s linear infinite;
+			margin: 10px; /* Add margin to create the light margin effect */
+		}
 
-	position: absolute;
-	top: 25%;
-	color:#fff;
-	margin-left: 300px;
-	
+		.lgin .box1::before,
+		.lgin .box1::after {
+			content: '';
+			position: absolute;
+			width: 100%;
+			height: 100%;
+			top: 0;
+			left: 0;
+			background: inherit;
+		}
 
-}
-.login.box{
-	justify-content: center;
-	position: relative;
-	width: 370px;
-	height: 450px;
-	background: white;
-	
-	overflow: hidden;
-	margin-top: 100px ;
-	margin-left: 600px;
-}
-
-
-
-.login.box::before{
-
-	content:'';
-	position: absolute;
-	top: -50%;
-	left: -50%;
-	width: 370px;
-	height: 450px;
-	background: linear-gradient(60deg,transparent,#453fff,#453fff);
-	transform-origin: bottom right;
-	animation: animate 6s linear infinite ;
-
-}
-
-
-.login.box::after{
-
-	content:'';
-	position: absolute;
-	top: -50%;
-	left: -50%;
-	width: 370px;
-	height: 450px;
-	background: linear-gradient(60deg,transparent,#453fff,#453fff);
-	transform-origin: bottom right;
-	animation: animate 6s linear infinite ;
-	animation-delay: -3s;
-
-}
-
-@keyframes animate{
-	0%{
-
-		transform: rotate(0deg);
-
-	}
-	100%{
-
-		transform: rotate(360deg);
-
-	}
-
-}
-
-.login form{
-
-	position:absolute;
-	inset: 2px;
-	
-	background: white;
-	z-index: 10;
-	padding: 30px 30px;
-	display: flex;
-	flex-direction: column;
-
-}
-
-.login #role {
-    width: 200px;
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #f7f5f2;
-    border-radius: 5px;
-    background-color: #f7f5f2;
-    color: #23242a;
-    margin-top: 15px;
-}
+		
+				
+		@keyframes animate {
+			0% {
+				background-position: 100% 0;
+			}
+			100% {
+				background-position: -100% 0;
+			}
+		}
 
 
-.login h2{ 
-	color: #23242a;
-	font-size: 35px;
-	font-weight: 500;
-	text-align: center;
+		.lgin form {
+			position: absolute;
+			inset: 2px;
+			background: white;
+			z-index: 10;
+			padding: 30px 30px;
+			display: flex;
+			flex-direction: column;
+		}
 
-}
+		.lgin .btn{
+			display: inline-block;
+			background: var(--black);
+			margin-top: 5rem;
+			color:var(--white);
+			font-size: 1.7rem;
+			padding:1rem 3rem;
+			cursor: pointer;
+			}
 
-.login.inputBox{
-	position: relative;
-	width: 300px;
-	margin-top: 35px;
-}
-
-.login.inputBox input{
-	position: relative;
-	width: 100%; 
-	padding: 20px 10px 10px;
-	background: transparent;
-	border: none;
-	outline: none;
-	color:#23242a;
-	font-size:1em;
-	letter-spacing: 0.05em;
-	z-index: 10;
-}
-
-.login input[type="submit"]{
-
-	font-size: 20px;
-	border: none;
-	outline: none;
-	color:white;
-	background: #000040;
-	padding: 5px;
-	margin-top:40px;
-	border-radius:90px;
-	font-weight:600;
-	cursor:pointer;
-	
-
-}
-
-.login input[type="submit"]:active{
-	background: linear-gradient(90deg,#453fff,#453fff);
-	opacity: 0.8;
-}
-
-.login .inputBox span{
-	position: absolute;
-	left: 0;
-	padding: 20px 10px 10px;
-	font-size: 1em;
-	color:#23242a;
-	pointer-events: none;
-	letter-spacing: 0.05em;
-	transition: 0.5s;
-}
-
-.login .inputBox input:valid ~ span,
-.login .inputBox input:focus ~ span
-
-{
-	color:#23242a;
-	transform: translateX(-10px) translateY(-30px);
-	font-size: 0.75em;
-}
-
-.login .inputBox i{
-	position: absolute;
-	left: 0;
-	bottom: 0;
-	width: 100%;
-	height: 2px;
-	background: #f7f5f2;
-	border-radius: 4px;
-	transition: 0.5s;
-	pointer-events: none;
-	z-index: 9;
-}
-
-.login .inputBox input:valid ~ i,
-.login .inputBox input:focus ~ i
-{
-	height: 44px;
-} 
-
-.login .links{
-	display: flex;
-	justify-content: space-between;
-
-}
-.login .links a{
-	margin: 25px 0;
-	font-size: 1em;
-	color: #23242a;
-	text-decoration: none;
-
-}
-.login .links a:hover,
-.login .links a:nth-child(2){
-	color: #23242a;
-}
-
-.login .links a:nth-child(2):hover{
-	text-decoration:underline 2px;
-	text-underline-offset: 5px;
-}
+		.btn:hover{
+			background: var(--main-color);
+		}
 
 
+		.lgin #role {
+			width: 200px;
+			padding: 10px;
+			font-size: 16px;
+			border: 1px solid #f7f5f2;
+			border-radius: 5px;
+			background-color: #f7f5f2;
+			color: #23242a;
+			margin-top: 3rem;
+		}
 
-</style>
+		.lgin h1 {
+			color: #23242a;
+			font-size: 50px;
+			font-weight: 500;
+			text-align: center;
+			text-decoration: underline;
+		}
 
+		.lgin .inputBox1 {
+			position: relative;
+			width: 300px;
+			margin-top: 35px;
+			background-color: #eee;
+		}
 
+		.lgin .inputBox1 input {
+			position: relative;
+			width: 100%;
+			padding: 20px 10px 10px;
+			background: transparent;
+			border: none;
+			outline: none;
+			color: #23242a;
+			font-size: 1em;
+			letter-spacing: 0.05em;
+			z-index: 10;
+		}
 
+		
 
+		.lgin .inputBox1 span {
+			position: absolute;
+			left: 0;
+			padding: 20px 10px 10px;
+			font-size: 1.7rem;
+			color: #23242a;
+			pointer-events: none;
+			letter-spacing: 0.05em;
+			transition: 0.5s;
+		}
 
+		.lgin .inputBox1 input:valid ~ span,
+		.lgin .inputBox1 input:focus ~ span {
+			color: #23242a;
+			transform: translateX(-10px) translateY(-30px);
+			font-size: 0.75em;
+		}
+
+		.lgin .inputBox1 i {
+			position: absolute;
+			left: 0;
+			bottom: 0;
+			width: 100%;
+			height: 2px;
+			background: #f7f5f2;
+			border-radius: 4px;
+			transition: 0.5s;
+			pointer-events: none;
+			z-index: 9;
+		}
+
+		.lgin .inputBox1 input:valid ~ i,
+		.lgin .inputBox1 input:focus ~ i {
+			height: 44px;
+		}
+
+		.lgin .links {
+			display: flex;
+			justify-content: space-between;
+		}
+
+		.lgin .links a {
+			margin: 25px 0;
+			font-size: 1.7rem;
+			color: var(--main-color);
+			text-decoration: underline;
+		}
+
+		.lgin .links a:hover,
+		.lgin .links a:nth-child(2) {
+			color:  var(--black);
+		}
+
+		.lgin .links a:nth-child(2):hover {
+			text-decoration: underline 2px;
+			text-underline-offset: 5px;
+		}
+	</style>
 </head>
-
-	
 
 
 <body>
+
+
 	<!-- header section starts -->
 	@include('header')
 	<!-- header section ends -->
 
-	<!-- Your content section starts here -->
-	<section class="login">
-		<div class="imgBx">
-		<img src="{{ asset('storage/images/site images/home8.jpg') }}">
 
+
+
+
+  
+
+	
+	<section class="lgin">
+
+	
+		<div class="imgBx" style="background:url('{{asset('storage/images/site images/6.jpg')}}')no-repeat">
+		
+			<div class="text1">
+				<h1>Welcome to UniLinkUp</h1>
+				<h3>"Welcome to our UniLinkUp ! Connect, share, and engage with fellow scholars to enrich your academic journey here."</h3>
+			</div>
 		</div>
-		<div class="text">
-			<h4>Welcome to LinkUp</h4>
-		</div>
-		<div class="box">
+
+
+		<div class="box1">
+			
 			<form action="">
-				<h2>Sign In</h2>
-				<div class="inputBox">
+				<h1>Sign In</h1>
+				<div class="inputBox1">
 					<input type="text" required>
 					<span>Username</span>
 					<i></i>
 				</div>
-				<div class="inputBox">
+				<div class="inputBox1">
 					<input type="password" required>
 					<span>Enter Password</span>
 					<i></i>
 				</div>
+
+				
 				<select id="role">
-					<option value="country">Select Role</option>
-					<option value="Editor">Editor</option>
-					<option value="Moderator">Moderator</option>
-					<option value="Admin">Admin</option>
-					<option value="Student">Student</option>
+					<option vlaue="roles">Select Role</option>
+					<option vlaue="Editor">Editor</option>
+					<option vlaue="Moderator">Moderator</option>
+					<option vlaue="Admin">Admin</option>
+					<option vlaue="Student">Student</option>
+
+
 				</select>
-				<input type="submit" class="btn" value="Login">
+
+
+
+
+				<button class="btn" style="margin-left: 1rem;" type="submit">Log In</button>
+				<!-- <input type="submit" class="btn" value="Login"> -->
 				<div class="links">
-					<a href="{{ url('forget') }}">Forgot Password?</a>
-					<a href="#">SignUp</a>
+					<a href="<?=url('forget')?>">Fogot Password?</a>
+					
+
 				</div>
 			</form>
+
+		
+
 		</div>
-	</section>
-	<!-- Your content section ends here -->
+</section>
+
 
 	<!-- footer section starts -->
 	@include('footer')
