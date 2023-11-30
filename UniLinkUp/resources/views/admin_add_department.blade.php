4c70f051244f327b007481246a260ab1fb3d11fb
jayaniThathsarani <jayanithathsarani@gmail.com>
2023-11-30 12:23:10 +0530

    Update admin_add_department.blade.php

--- UniLinkUp/resources/views/admin_add_department.blade.php
+++ UniLinkUp/resources/views/admin_add_department.blade.php
@@ -122,13 +122,17 @@
 <div class="main-content">
     
     <div class="back" >
-        <a href="<?=url('admin')?>" class="btn" style="margin-bottom: 20px;">Back</a>
+        <a href="<?=url('admin_department')?>" class="btn" style="margin-bottom: 20px;">Back</a>
     </div>
 
     <div class="container">
         <h1>Add New Department</h1>
 
-        
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success')}}
+            </div>
+        @endif   
 
             <form action="{{route('departmentInput') }}" method="post">
              @csrf
