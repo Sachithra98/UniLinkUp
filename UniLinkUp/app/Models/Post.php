d075cf255b80203833402f1cd0e895839b15ff1d
Dhilmi-Samadhini <143329342+Dhilmi-Samadhini@users.noreply.github.com>
2023-12-05 00:39:36 +0530

    post.php & post operation.php content

--- UniLinkUp/app/Models/Post.php
+++ UniLinkUp/app/Models/Post.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//dhilmi
 class Post extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'Post_Id';
+
+    public function postOperations()
+    {
+        return $this->hasMany(PostOperation::class, 'Post_Id', 'Post_Id');
+    }
+
 }
--- UniLinkUp/app/Models/Post_Operation.php
+++ UniLinkUp/app/Models/Post_Operation.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//dhilmi
 class Post_Operation extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'Student_Id';
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'Post_Id', 'Post_Id');
+    }
 }
