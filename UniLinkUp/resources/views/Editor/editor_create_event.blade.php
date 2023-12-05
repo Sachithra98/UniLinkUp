e970afd278a99f7a4604785e0b505f18e76ebf99
Sachithra98 <sachithraw98@gmail.com>
2023-12-05 12:15:26 +0530

    Update editor_create_event.blade.php

--- UniLinkUp/resources/views/Editor/editor_create_event.blade.php
+++ UniLinkUp/resources/views/Editor/editor_create_event.blade.php
@@ -1,4 +1,4 @@
-<!-- sachithra-start -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -72,6 +72,14 @@
             border-radius: 4px;
         }
 
+        .main-content .container textarea {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 4px;   
+        }
+
         /* Style file input fields */
         .main-content .container input[type="file"] {
             width: 100%;
@@ -134,7 +142,8 @@
    
 
 <div class="main-content">
-    
+
+
     <div class="back" >
         <a href="<?=url('editor')?>" class="btn" style="margin-bottom: 20px;">Back</a>
     </div>
@@ -142,34 +151,62 @@
     <div class="container">
         <h1>Create New Event</h1>
 
-        <form action="{{ url('/postsInput') }}" method="post" enctype="multipart/form-data">
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success')}}
+            </div>
+        @endif
 
+            <form action="{{url('/posts/store')}}" method="post" enctype="multipart/form-data">
                 @csrf
                 
 
                 <div class="form-group">
+                    <label for="post_title">Editor_Id:</label>
+                    <input type="text" id="Editor_Id" name="Editor_Id" value="{{ old('Editor_Id') }}" required>
+                    @error('Editor_Id')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
+                </div>
+                
+                <div class="form-group">
                     <label for="post_title">Post Title:</label>
-                    <input type="text" id="post_title" name="post_title" required>
+                    <input type="text" id="post_title" name="post_title" value="{{ old('post_title') }}" required>
+                    @error('post_title')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
 
                 <div class="form-group">
-                    <label for="post_desc">Post Description:</label>
-                    <input type="text" id="post_desc" name="post_desc" required>
+                    <label for="post_desc">Description:</label>
+                    <textarea class="desc" id="desc" name="desc" rows="10" required>{{ old('desc') }}</textarea>
+                    @error('desc')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="exp date">Expire Date:</label>
-                    <input type="text" id="exp date" name="exp date" required>
+                    <input type="date" id="exp_date" name="exp_date" value="{{ old('exp_date') }}" required>
+                    @error('exp_date')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="">Post Media:</label>
-                    <input type="file" id="post" name="post" multiple required>
+                    <input type="file" id="ppost" name="ppost" value="{{ old('ppost') }}" multiple required>
+                    @error('ppost')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="approval">Approval letter:</label>
-                    <input type="file" id="approval" name="approval" multiple required>
+                    <input type="file" id="approval" name="approval" value="{{ old('approval') }}" multiple required>
+                    @error('approval')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
 
 
@@ -182,85 +219,92 @@
                     <label for="faculty_society">Faculty Society</label><br><br>
                 </div>
 
-                 <!--<div class="form-group faculty-options">
-                    <label for="faculty">Faculty:</label>
-                    <select>
-                        <option>Faculty of Science</option>
-                        <option>Faculty of Engineering</option>
-                        <option>Faculty of Medicine</option>
-                        <option>Faculty of Technology</option>
-                        <option>Faculty of Agriculture</option>
-                        <option>Faculty of Allied Health Science</option>
-                        <option>Faculty of Humanitieas and Social Sciences</option>
-                        <option>Faculty of Management</option>
-                        <option>Faculty of Fisheries and Marine Science</option>
+
+                <div class="form-group">
+                    <label for="fac_id">Faculty:</label>
+                    <select name="fac_id" id="fac_id" >
+                        <option value="">Select Faculty:</option>
+                        @foreach($faculties as $row1)
+                            <option value="{{ $row1->Faculty_Id }}" {{ old('fac_id') == $row1->Faculty_Id ? 'selected' : '' }}>
+                                {{ $row1->Faculty_Name }}
+                            </option>
+                        @endforeach
                     </select>
+                    @error('fac_id')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
 
-                <div class="form-group university-society-options">
-                    <label for="society">Society:</label>
-                    <select>
-                        <option>Leo club</option>
-                        <option>AIESEC</option>
-                        <option>Buddhist Society</option>
-                        <option>Rotaract Club</option>
-                        <option>Catheolic Society</option>
-                        <option>Chemistry Society</option>
-                        <option>Mathematical & Statistics Society</option>
-                    </select>
-                </div>-->
 
-                <!-- <div class="form-group">
-                    <label for="dep id">Department ID:</label>
-                    <input type="text" id="dep id" name="dep id" required>
+
+
+                <div class="form-group">
+                    <label for="soc_id">Society:</label>
+                    <select name="soc_id" id="soc_id"  >
+                        <option value="">Select Society :</option>
+                        @foreach($societies as $row2)
+                            <option value="{{ $row2->Society_Id}}" {{ old('soc_id') == $row2->Society_Id ? 'selected' : '' }}>
+                                {{ $row2->Society_Name}}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('soc_id')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
-              -->
 
                 <div class="form-group">
                     <label for="dep_id">Department:</label>
-                    <select name="dep_id" id="dep_id" required>
+                    <select name="dep_id" id="dep_id" value="{{ old('dep_id') }}" >
                         <option value="">Select Department :</option>
                         @foreach($departments as $row3)
-                            <option>{{ $row3->Dep_Name }}</option>
+                            <option value="{{ $row3->Dep_Id }}" {{ old('dep_id') == $row3->Dep_Id ? 'selected' : '' }}>
+                                {{ $row3->Dep_Name }}
+                            </option>
                         @endforeach
                     </select>
-
-                </div>
-
-             
-            
-    <script>
+                    @error('dep_id')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
+                </div> 
                 
-    $(document).ready(function() {
-        const universitySocietyRadio = $('#university_society');
-        const facultySocietyRadio = $('#faculty_society');
-        const universitySocietyOptions = $('.university-society-options');
-        const facultyOptions = $('.faculty-options');
-        const universitySocietySelect = $('#universitySocietySelect');
-        const facultySelect = $('#facultySelect');
+            
+            
+<script>
+                
+                <script>
+                
+                $(document).ready(function() {
+                    const universitySocietyRadio = $('#university_society');
+                    const facultySocietyRadio = $('#faculty_society');
+                    const universitySocietyOptions = $('.university-society-options');
+                    const facultyOptions = $('.faculty-options');
+                    const universitySocietySelect = $('#universitySocietySelect');
+                    const facultySelect = $('#facultySelect');
+            
+                    universitySocietyRadio.change(function() {
+                        if (universitySocietyRadio.is(':checked')) {
+                            universitySocietyOptions.show();
+                            facultyOptions.hide();
+                            facultySelect.prop('disabled', true);
+                            universitySocietySelect.prop('disabled', false);
+                        }
+                    });
+            
+                    facultySocietyRadio.change(function() {
+                        if (facultySocietyRadio.is(':checked')) {
+                            universitySocietyOptions.show();
+                            facultyOptions.show();
+                            facultySelect.prop('disabled', false);
+                            universitySocietySelect.prop('disabled', false);
+                        }
+                    });
+                });
+            </script>
 
-        universitySocietyRadio.change(function() {
-            if (universitySocietyRadio.is(':checked')) {
-                universitySocietyOptions.show();
-                facultyOptions.hide();
-                facultySelect.prop('disabled', true);
-                universitySocietySelect.prop('disabled', false);
-            }
-        });
-
-        facultySocietyRadio.change(function() {
-            if (facultySocietyRadio.is(':checked')) {
-                universitySocietyOptions.show();
-                facultyOptions.show();
-                facultySelect.prop('disabled', false);
-                universitySocietySelect.prop('disabled', false);
-            }
-        });
-    });
-</script>
                 
            
-                <div class="new_society" style="text-align: center; display: flex; justify-content: center; align-items: center; padding-top: 5rem; padding-bottom: 5rem;">
+                <div class="new_event" style="text-align: center; display: flex; justify-content: center; align-items: center; padding-top: 5rem; padding-bottom: 5rem;">
                             <button class="btn" style="margin-left: 1rem;" type="reset">Reset</button>
                             <button class="btn" style="margin-left: 1rem;" type="submit">Request for Create Event</button> 
                 </div>
@@ -280,4 +324,3 @@
     @endsection
 </body>
 </html>
-<!-- sachithra-end -->
