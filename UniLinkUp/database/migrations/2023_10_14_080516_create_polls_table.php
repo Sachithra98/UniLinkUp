7eeb053687d9351ee732038938edf0748163ee13
Sachithra98 <sachithraw98@gmail.com>
2023-12-10 10:58:37 +0530

    Update 2023_10_14_080516_create_polls_table.php

--- UniLinkUp/database/migrations/2023_10_14_080516_create_polls_table.php
+++ UniLinkUp/database/migrations/2023_10_14_080516_create_polls_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('polls', function (Blueprint $table) {
-            $table->id('poll_id');
+            $table->id();
             $table->string('poll_title');
             $table->string('poll_desc')->nullable();
             //$table->binary('media_path')->nullable();
