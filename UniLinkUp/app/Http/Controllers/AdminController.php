24a629d16016a2f8c39b34b783bf958d77779893
Piyumi Nisansala <piyumi479@gmail.com>
2023-12-15 03:25:16 +0530

    Update AdminController.php

--- UniLinkUp/app/Http/Controllers/AdminController.php
+++ UniLinkUp/app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Admin;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use DB;
 
 class AdminController extends Controller
 {
@@ -30,4 +31,28 @@ class AdminController extends Controller
         return redirect('/admin_createaccA')->with('success','Data successfully added!');
     } 
     //jayani-ends
+
+
+    //piyumi start
+    public function edit_record($Admin_Id){
+        $admin = Admin::where('Admin_Id', $Admin_Id)->first();
+        return view('/Admin/admin_edit_UMA', compact('admin'));
+    }
+    
+
+    public function update_data(Request $request,  $Admin_Id) {
+        $admin = Admin::find($Admin_Id);
+       
+    
+        if (!$admin) {
+            return redirect()->back()->with('error', 'Admin not found');
+        }
+        
+        $admin->Admin_Name = $request->input('Admin_Name');
+        $admin->email= $request->input('email');
+        $admin->save();
+    
+        return redirect('/Admin_UMA')->with('success', 'Admin data successfully updated!');
+    }
+
 }
