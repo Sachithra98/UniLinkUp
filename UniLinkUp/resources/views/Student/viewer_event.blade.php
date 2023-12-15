a84398900157ebb9505efea858255d16a23fbefe
DTharendra <143478569+DTharendra@users.noreply.github.com>
2023-12-15 12:01:54 +0530

    event download

--- UniLinkUp/resources/views/Student/viewer_event.blade.php
+++ UniLinkUp/resources/views/Student/viewer_event.blade.php
@@ -147,6 +147,8 @@
                 <button class="btn">Like <i class="fa fa-thumbs-up"></i></button>
                 <button class="btn">Dislike <i class="fa fa-thumbs-down"></i></button>
                 <button class="btn">Share <i class="fa fa-share-alt"></i></button>
+                <a href="{{ asset('/uploads/' . $post->media_path) }}" class="btn" download>Download <i class="fa fa-download"></i></a>
+
                 </div>
         </div>
         @endforeach
