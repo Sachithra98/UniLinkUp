002c38a118b62e6f9b73d61504f22dd514e18b55
Sachithra98 <sachithraw98@gmail.com>
2023-12-16 01:20:15 +0530

    migration

---
+++ UniLinkUp/database/migrations/2023_12_12_200422_create_publish_notices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publish_notices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('Publish_notice_id')->constrained('notices','Post_Id');
+            $table->string('Editor_Id');
+            $table->string('Title');
+            $table->text('Description');
+            $table->string('media_path')->nullable();
+            $table->string('Approval_Letter')->nullable();
+            $table->string('Society_Id')->nullable();
+            $table->string('Dep_Id')->nullable();
+            $table->string('Faculty_Id')->nullable();
+    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publish_notices');
+    }
+};
