58e8b5869b056ace6a760e9104296e7cf956d667
Shalini <shalinikavindya1@gmail.com>
2023-12-15 14:32:33 +0530

    Update Vote.php

--- UniLinkUp/app/Models/Vote.php
+++ UniLinkUp/app/Models/Vote.php
@@ -12,8 +12,14 @@ class Vote extends Model
 
     protected $fillable = ['choice', 'poll_id'];
 
+    protected $primaryKey = 'vote_id';
+
     public function publishPoll()
     {
         return $this->belongsTo(PublishPoll::class, 'poll_id');
+
+        return $this->belongsTo(PublishPoll::class, 'poll_id', 'poll_id');
+
     }
+
 }
