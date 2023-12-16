e4782ae0af2346e64a52bf6aefd93b2d1528ade1
Sachithra98 <sachithraw98@gmail.com>
2023-12-16 23:02:24 +0530

    error

--- UniLinkUp/resources/views/Student/share_notice.blade.php
+++ UniLinkUp/resources/views/Student/share_notice.blade.php
@@ -82,7 +82,7 @@
 	<!-- header section ends -->
 
     <!-- taskbar section starts -->
-    @include('Viewer/viewer_task')
+    @include('Student/viewer_task')
 	<!-- taskbar section ends -->
 
 
--- UniLinkUp/resources/views/Student/viewer_poll.blade.php
+++ UniLinkUp/resources/views/Student/viewer_poll.blade.php
@@ -123,7 +123,7 @@
 	<!-- header section ends -->
 
     <!-- taskbar section starts -->
-    @include('Viewer/viewer_task')
+    @include('VieStudentwer/viewer_task')
 	<!-- taskbar section ends -->
 
 
