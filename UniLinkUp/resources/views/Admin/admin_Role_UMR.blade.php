c369d958e3f4292f9214f86d7876d8bfd1041824
jayaniThathsarani <jayanithathsarani@gmail.com>
2023-12-17 14:18:54 +0530

    updated

--- UniLinkUp/resources/views/Admin/admin_Role_UMR.blade.php
+++ UniLinkUp/resources/views/Admin/admin_Role_UMR.blade.php
@@ -75,7 +75,7 @@
 <div class="main-content">
 
     <div class="back" >
-        <a href="{{route('admin.dashboard')}}" class="btn" style="margin-bottom: 20px;">Back</a>
+        <a href="{{route('admin_dashboard')}}" class="btn" style="margin-bottom: 20px;">Back</a>
     </div>
 
     <div class="container">
